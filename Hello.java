ac1be13fceb85b8c30db3e444b18033b81b381b7
Prem Lond <120458594+Prem-Lond@users.noreply.github.com>
2023-10-14 23:50:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("hello Word");
-        System.out.println("My Name is Prem");p
+        System.out.println("My Name is Prem");
     }
 }
